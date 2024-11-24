5ed92eb996dc2f93381e1b00128306bb55a7de17
SanthoshSandy04 <Santhosh@crayond.co>
2024-11-24 13:40:53 +0530

    Changes added

--- src/index.ts
+++ src/index.ts
@@ -19,77 +19,51 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 
-
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 io.on("connection", (socket: Socket) => {
-    console.log("User connected:", socket.id);
-  
-    socket.on("joinRoom", ({ username, room }: RoomData) => {
-      // Save user data
-      users[socket.id] = { username, room };
-  
-      // Join the room
-      socket.join(room);
-  
-      // Emit personalized message to the user who joined
-      socket.emit("message", {
-        username: "System",
-        text: `You have joined the ${room} room.`,
-        time: new Date().toLocaleTimeString(),
-      });
-  
-      // Broadcast to others in the room
-      socket.to(room).emit("message", {
-        username: "System",
-        text: `${username} has joined the room.`,
-        time: new Date().toLocaleTimeString(),
-      });
-  
-      // Update room user list for everyone in the room
-      io.in(room).emit("roomUsers", {
-        room,
-        users: Object.values(users).filter((user) => user.room === room),
-      });
+  // When a user joins
+  socket.on("joinRoom", ({ username, room }: RoomData) => {
+    users[socket.id] = { username, room };
+    rooms.add(room);
+
+    socket.join(room);
+
+    // Send system message about room joining
+    socket.emit("message", {
+      text: `You have joined the ${room} chat room.`,
+      type: "system",
+      time: new Date().toLocaleTimeString(),
     });
-  
-    // Handle messages
-    socket.on("chatMessage", (msg) => {
-      const user = users[socket.id];
-      if (user) {
-        io.in(user.room).emit("message", {
-          username: user.username,
-          text: msg,
-          time: new Date().toLocaleTimeString(),
-        });
-      }
+
+    // Notify other users in the room
+    socket.to(room).emit("message", {
+      text: `${username} has joined the room.`,
+      type: "system",
+      time: new Date().toLocaleTimeString(),
     });
-  
-    // Handle user disconnecting
-    socket.on("disconnect", () => {
-      const user = users[socket.id];
-      if (user) {
-        // Notify others in the room
-        socket.to(user.room).emit("message", {
-          username: "System",
-          text: `${user.username} has left the room.`,
-          time: new Date().toLocaleTimeString(),
-        });
-  
-        // Remove the user
-        delete users[socket.id];
-  
-        // Update room user list
-        io.in(user.room).emit("roomUsers", {
-          room: user.room,
-          users: Object.values(users).filter((u) => u.room === user.room),
-        });
-      }
+
+    io.in(room).emit("roomUsers", {
+      room,
+      users: Object.values(users).filter((user) => user.room === room),
     });
   });
-  
+
+  // Handle chat messages
+  socket.on("chatMessage", (msg) => {
+    const user = users[socket.id];
+    if (user) {
+      io.in(user.room).emit("message", {
+        username: user.username,
+        text: msg,
+        type: "user", // Mark the message as a user message
+        time: new Date().toLocaleTimeString(),
+      });
+    }
+  });
+});
 
 server.listen(3500, () => {
   console.log("Server running on http://localhost:3500");
